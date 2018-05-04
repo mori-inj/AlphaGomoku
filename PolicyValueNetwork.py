dbfb4a20e642bac3414ef37d3ebe604cbc4d9f6d
mori-inj <leokim1022@snu.ac.kr>
2018-05-04 18:28:54 +0900

    network train and get output

--- PolicyValueNetwork.py
+++ PolicyValueNetwork.py
@@ -7,29 +7,34 @@ def ConvBlock(x):
     x = tf.nn.relu(x)
     return x
 
-def ResidualBlock(x_):
+def ResidualBlock(h):
     with tf.variable_scope('res'):
-        x = tf.layers.conv2d(x_, 256, 3, strides=1, padding='same', name='conv1')
+        x = tf.layers.conv2d(h, 256, 3, strides=1, padding='same', name='conv1')
         x = tf.layers.batch_normalization(x, name='bn1')
         x = tf.nn.relu(x)
         x = tf.layers.conv2d(x, 256, 3, strides=1, padding='same', name='conv2')
         x = tf.layers.batch_normalization(x, name='bn2')
-        x = x + x_
+        x = x + h
         x = tf.nn.relu(x)
         return x
 
-def PolicyOutputBlock(x, output_size):
+def PolicyOutputBlock(x, board_size, output_size):
     with tf.variable_scope('policy_output'):
-        x = tf.layers.conv2d(x, 2, 1, strides=1, padding='same', name='conv')
+        x = tf.layers.conv2d(x, 256, 1, strides=1, padding='same', name='conv')
         x = tf.layers.batch_normalization(x)
         x = tf.nn.relu(x)
-        x = tf.layers.dense(x, output_size)
+        x = tf.reshape(x, [-1, 256 * board_size**2])
+        x = tf.layers.dense(x, output_size, name='fc')
         return x
 
-def ValueOutputBlock(x):
+def ValueOutputBlock(x, board_size):
     with tf.variable_scope('value_output'):
         x = tf.layers.conv2d(x, 1, 1, strides=1, padding='same', name='conv')
+        
+        #x = tf.reshape(x, [-1, board_size**2])
+        x = tf.reshape(x, [-1, 1, 1, board_size**2])
         x = tf.layers.batch_normalization(x)
+        
         x = tf.nn.relu(x)
         x = tf.layers.dense(x, 256, name='fc1')
         x = tf.nn.relu(x)
@@ -48,27 +53,45 @@ class PolicyValueNetwork:
         self.g = tf.Graph()
         with self.g.as_default():
             self.X = tf.placeholder(tf.float32, [None, self.board_size, self.board_size, self.input_frame_num])
-            self.pi = tf.placeholder(tf.float32, [None, self.board_size*self.board_size])
+            self.pi = tf.placeholder(tf.float32, [None, self.board_size**2])
             self.Z = tf.placeholder(tf.float32, [None, 1])
 
-            self.X = ConvBlock(self.X)
+            X = ConvBlock(self.X)
+            
             for i in range(self.residual_num):
                 with tf.variable_scope('res_%d' % i):
-                    self.X = ResidualBlock(self.X)
-            self.P = PolicyOutputBlock(self.X, self.board_size*self.board_size)
-            self.V = ValueOutputBlock(self.X)
-            
+                    X = ResidualBlock(X)
+            self.P = PolicyOutputBlock(X, self.board_size, self.board_size**2)
+            self.V = ValueOutputBlock(X, self.board_size)
+           
+
             var = tf.trainable_variables()
             theta = tf.add_n([tf.nn.l2_loss(v) for v in var]) * 1e-4
-            loss = tf.reduce_mean((self.Z-self.V)**2 - self.pi * tf.log(self.P) + theta)
-            learning_rate = 0.02
+            self.value_loss = (self.Z - self.V)**2
+            self.policy_loss = - self.pi * tf.log(tf.maximum(self.P,1e-7))
+            self.loss = tf.reduce_mean(self.value_loss + self.policy_loss + theta)
+            
+            learning_rate = 0.02 # TODO apply learning step anneling
             optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9)
-            train_model = optimizer.minimize(loss)
+            self.train_model = optimizer.minimize(self.loss)
+            self.init = tf.global_variables_initializer()
 
-    def train(self,):
-        pass
+        self.sess = tf.Session(graph = self.g)
+        self.sess.run(self.init)
 
-    def get_output(self, ):
-        pass
+    def train(self, X_, pi_, Z_):
+        for i in range(100): # TODO should change # of iteration steps
+            self.sess.run(self.train_model, feed_dict={self.X: X_, self.pi: pi_, self.Z: Z_})
+            print(self.sess.run(self.loss, feed_dict={self.X: X_, self.pi: pi_, self.Z: Z_}))
+
+    def get_output(self, X_):
+        return self.sess.run([self.P, self.V], feed_dict={self.X: X_})
         
-
+"""
+PVN = PolicyValueNetwork(3,2,10)
+A = np.random.rand(18).reshape(1,3,3,2)
+B = np.random.rand(9).reshape(1,9)
+B = np.absolute(B)
+C = np.random.rand(1).reshape(1,1)
+PVN.train(A,B,C)
+"""
