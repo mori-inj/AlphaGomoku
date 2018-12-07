5fe8cef0f1a1e6061c13608236ec40968068a73c
mori-inj <leokim1022@snu.ac.kr>
2018-12-07 22:33:33 +0900

    temporary commit to share code

--- python/MCTSAgent.py
+++ python/MCTSAgent.py
@@ -21,6 +21,12 @@ class MCTSAgent:
         mcts = MCTS(BOARD_SIZE, EVALUATE)
         mcts.root = Node(board_state, EVALUATE)
 
+        if board_state.turn < TEMPER_THRES:
+                TEMPERATURE = 1
+        else:
+            TEMPERATURE = TEMPER_EPS
+
+
         for i in range(MCTS_SIM_NUM):
             mcts.search(mcts.root)
 
